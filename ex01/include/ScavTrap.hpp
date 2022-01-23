3768ba86136f8a6d7b900bae44de7369acfd29d5
takashi247 <takashi247@gmail.com>
2022-01-23 10:58:38 +0900

    add ex01

---
+++ ex01/include/ScavTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+# include <string>
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &other);
+		virtual ~ScavTrap();
+		ScavTrap	&operator=(const ScavTrap &other);
+		void		attack(std::string const &target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+	private:
+		static const unsigned int	HITPOINTS_ST = 100;
+		static const unsigned int	ENERGY_POINTS_ST = 50;
+		static const unsigned int	ATTACK_DAMAGE_ST = 20;
+};
+
+#endif
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,19 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int
+	main()
+{
+	ClapTrap	c1("Clap First");
+	ClapTrap	c2("Clap Second");
+	ScavTrap	s1("Scav First");
+	ScavTrap	s2("Scav Second");
+
+	s1.attack("Clap First");
+	c1.takeDamage(s1.getAttackDamage());
+	c1.beRepaired(50);
+	c2.beRepaired(1);
+	s2.attack("Scav First");
+	s1.takeDamage(s2.getAttackDamage());
+	s1.beRepaired(10);
+}
