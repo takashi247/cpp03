f9b838a7c98cd94da41ae1fcfb7cfcc7c1429d8d
takashi247 <takashi247@gmail.com>
2022-01-26 17:28:55 +0900

    add test cases for ex00

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -8,13 +8,17 @@ int
 	ClapTrap	second("second");
 	ClapTrap	third("third");
 	ClapTrap	third_cpy(third);
+    ClapTrap    fourth;
 
     std::cout << std::endl;
+    fourth = third;
 	first.attack("second");
 	second.takeDamage(first.getAttackDamage());
 	third.beRepaired(100);
 	second.beRepaired(1);
 	third_cpy.attack("first");
 	first.takeDamage(third_cpy.getAttackDamage());
+    fourth.attack("third");
+    third.takeDamage(fourth.getAttackDamage());
     std::cout << std::endl;
 }
